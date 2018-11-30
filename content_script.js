bc2f3554286c1e5185f2574f9b33600b571b6864
morix1500 <rade1500@gmail.com>
2018-11-30 13:26:26 +0900

    add interval

--- content_script.js
+++ content_script.js
@@ -5,4 +5,22 @@
   text = items.join(" ");
   
   $("textarea.compose-text").val("\n" + text);
+
+  let waitSec = 5;
+  let spanedSec = 0;
+
+  $(".js-send-button").on("click", function() {
+    let id = setInterval(function(){
+      spanedSec += 0.5;
+      if (spanedSec >= waitSec) {
+        clearInterval(id);
+	spanedSec = 0;
+      }
+      if ($("textarea.compose-text").val() == "") {
+        $("textarea.compose-text").val("\n" + text);
+        spanedSec = 0;
+        clearInterval(id);
+      }
+    }, 500);
+  });
 });
